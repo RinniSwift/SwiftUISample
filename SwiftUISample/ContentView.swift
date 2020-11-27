e3127994d95bc5f25cdd6c62f39c19f64131436f
Rinni Swift <rinradaswift@gmail.com>
2020-11-27 13:37:30 -0800

    [add] lazyvgrid as collectionview

--- SwiftUISample/ContentView.swift
+++ SwiftUISample/ContentView.swift
@@ -2,27 +2,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    private var sfSymbolNames = ["pencil.circle",
+                                 "folder.circle",
+                                 "paperplane.circle",
+                                 "tray.circle",
+                                 "tray.and.arrow.down",
+                                 "doc",
+                                 "calendar.badge.clock",
+                                 "arrowshape.zigzag.forward",
+                                 "book.circle",
+                                 "cloud.bolt.rain",
+                                 "cursorarrow.click.2"]
+    private var gridItemLayout = [GridItem(.flexible()), GridItem(.flexible()), GridItem(.flexible())]
+
     var body: some View {
         NavigationView {
             HStack {
-                VStack(alignment: .leading) {
-                    HStack(alignment: .center, spacing: 15, content: {
-                        Spacer()
-                        Image(systemName: "chevron.backward")
-                        Spacer()
-                        Text("Placeholder")
-                        Spacer()
-                        Image(systemName: "chevron.forward")
-                        Spacer()
-                    })
-
-                    Spacer()
-                    Text("Middle Text")
-                    Spacer()
-                    Text("Bottom Text")
-                    Spacer()
-                }
-                .preferredColorScheme(.dark)
+                ScrollView {
+                    LazyVGrid(columns: gridItemLayout, spacing: 15) {
+                        ForEach((sfSymbolNames.indices), id: \.self) {
+                                Image(systemName: sfSymbolNames[$0])
+                                    .font(.system(size: 30))
+                                    .frame(width: 80, height: 80)
+                                    .background(Color(.systemYellow))
+                                    .cornerRadius(8)
+                            }
+                        }
+                    }
             }
             .navigationTitle("Instagram")
         }
